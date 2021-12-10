5a1c67b98caa9e0d4150a25c5786b729220bc7c2
xingzhaoyang <xingzhaoyang@invoo.com.cn>
2021-12-10 09:58:20 +0800

    时隔十五天，我又开始把学习Java的计划提上日程了，不知道这次能坚持多久，希望我可以学好Java

---
+++ src/main/java/com/example/basics/javaexc/Jave1210.java
@@ -0,0 +1,9 @@
+package com.example.basics.javaexc;
+
+public class Jave1210 {
+/*java 运行原理：Java是高级语言，是最接近人类语言的
+*
+* 1.首先，通过Javac把代码编译成计算机能识别的二进制语言，然后在JRE里边的jvm虚拟机里去运行
+* 2.其次，Java是跨平台的，跨平台的意思是：Java开发时必须需要安装Java的开发环境，
+* */
+}
