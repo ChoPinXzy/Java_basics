aa6e59ff27b4e2f5501a18c2a1e0acf18ce8f4e7
xingzhaoyang <xingzhaoyang@invoo.com.cn>
2021-12-10 10:28:18 +0800

    坚持下去

---
+++ src/main/java/com/example/basics/javaexc/demo1210.java
@@ -0,0 +1,28 @@
+package com.example.basics.javaexc;
+
+public class demo1210 {
+/*一、java 运行原理：Java是高级语言，是最接近人类语言的
+*
+* 1.首先，通过Javac把代码编译成计算机能识别的二进制语言，然后在JRE里边的jvm虚拟机里去运行
+* 2.其次，Java是跨平台的，跨平台的意思是：Java开发时必须需要安装Java的开发环境——jdk，比如里边的Javac就是编译我们写的代码的。
+* 然后编译好的代码，会放到JRE——Java的运行环境中，JRE里边又JVM虚拟机和类库（java 自己提供的一些方法）比如说：windows有Windows的jre运行库，Linux有Linux的，mac有mac的，Java就是通过JRE不同的运行环境实现跨平台的，即一套代码多个平台都可以运行
+* 3.Java通过编译后，代码变成了计算机可以识别的语言，然后命令计算机去做事，其实就是二进制0和1，计算机硬件可以理解为很多开关，0是开，1是关，编译后的语言就是二进制的命令
+*
+*
+* */
+    /*二、配置Java的path路径以及path环境变量的知识
+    1.命令之所以能在命令窗口中执行，是因为path环境变量已经把这些命令配置好了
+比如Java和javac之所以在任何命令窗口中能够运行，就是因为path变量配置好了，程序执行的时候可以找到javac等
+
+在配置Java环境变量的时候，因为Java在jdk的bin目录下，所以需要在系统环境变量中，配置一个Java_home:里边写jdk的bin目录路径，把%Java_home%bin写在path的路径的好处就是，之后切换了Java的jdk版本，可以不用修改path的变量
+
+
+    *
+    * */
+
+
+    /*
+    intellij IDEA （智能理念）的使用技巧
+    * */
+
+}
